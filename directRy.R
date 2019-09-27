2c15f5df3ac4b599f50e26dba68cc830dd3f77cc
M <35826002+mikew93@users.noreply.github.com>
2019-09-26 19:11:57 -0600

    Create directRy.R

---
+++ directRy.R
@@ -0,0 +1,17 @@
+#' Pastes Directory Path
+#' @param mainDirect A string
+#' @return A string
+#' @export
+
+directRy <- function(mainDirect)
+{
+  if(class(mainDirect) != "character")
+  {
+    stop('~~~mainDirect must be a string "like this"~~~')
+  }else{
+    mainDirect <- as.character(mainDirect)
+  }
+  return(mainDirect)
+}
+
+# (GNU3 c) 2019 John Michael Adrian Wojahn
