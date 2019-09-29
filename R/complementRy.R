1149fa057715b6ef33eea6ea0d07e1827fc974a5
M <35826002+mikew93@users.noreply.github.com>
2019-09-29 00:55:26 -0600

    Create complementRy.R

---
+++ R/complementRy.R
@@ -0,0 +1,28 @@
+#' Get complement
+#' @param seq sequence
+#' @return comp
+#' @export
+
+complementRy <- function(seq)
+{
+  seq <- as.character(seq)
+  comp <- as.data.frame(matrix(nrow=length(seq),ncol=1))
+  for(i in 1:nrow(comp))
+  {
+    if(seq[i] == "A")
+    {
+      comp[i,1] <- as.character("T")
+    }else if(seq[i] == "T"){
+      comp[i,1] <- as.character("A")
+    }else if(seq[i] == "G"){
+      comp[i,1] <- as.character("C")
+    }else if(seq[i] == "C"){
+      comp[i,1] <- as.character("G")
+    }
+    comp <- paste(comp, collapse = "")
+    comp <- as.character(comp)
+    comp <<- comp
+    return(comp)
+  }
+}
+# (c) 2019 John Michael Adrian Wojahn
