7fdbbb271b8ea21d63b2b3f338e70c3483c2f9c1
M <35826002+mikew93@users.noreply.github.com>
2019-09-29 01:23:56 -0600

    Update revcomplementRy.R

--- R/revcomplementRy.R
+++ R/revcomplementRy.R
@@ -7,23 +7,34 @@ revcomplementRy <- function(seq)
 {
   seq <- as.character(seq)
   seq <- as.character(toupper(seq))
-  comp <- as.data.frame(matrix(nrow=length(seq),ncol=1))
-  for(i in 1:nrow(comp))
+  seq <- Biostrings::strsplit(seq,"")
+  seq <- as.data.frame(seq)
+  comp <- as.data.frame(matrix(nrow=nrow(seq),ncol=1))
+  for(k in 1:nrow(comp))
   {
-    if(seq[i] == "A")
+    if(as.character(seq[k,1]) == "A")
     {
-      comp[as.numeric(nrow(comp) + 1 - i),1] <- as.character("T")
-    }else if(seq[i] == "T"){
-      comp[as.numeric(nrow(comp) + 1 - i),1] <- as.character("A")
-    }else if(seq[i] == "G"){
-      comp[as.numeric(nrow(comp) + 1 - i),1] <- as.character("C")
-    }else if(seq[i] == "C"){
-      comp[as.numeric(nrow(comp) + 1 - i),1] <- as.character("G")
+      comp[as.numeric(nrow(comp) + 1 - k),1] <- as.character("T")
+    }else if(as.character(seq[k,1] == "T")){
+      comp[as.numeric(nrow(comp) + 1 - k),1] <- as.character("A")
+    }else if(as.character(seq[k,1] == "G")){
+      comp[as.numeric(nrow(comp) + 1 - k),1] <- as.character("C")
+    }else if(as.character(seq[k,1] == "C")){
+      comp[as.numeric(nrow(comp) + 1 - k),1] <- as.character("G")
     }
   }
-    comp <- paste(comp, collapse = "")
+  out <- ""
+  for(k in 1:nrow(comp))
+  {
+    tmp <- as.character(comp[k,1])
+    out <- as.character(paste(out,tmp,sep=""))
+  }
+    comp <- as.character(out)
     comp <- as.character(comp)
     comp <<- comp
     return(comp)
 }
 # (c) 2019 John Michael Adrian Wojahn
+
+  
+  
