d98f5f436e32526bddb477336c95566e3ee4751e
M <35826002+mikew93@users.noreply.github.com>
2019-10-03 14:53:35 -0600

    Create isodd.R

---
+++ R/isodd.R
@@ -0,0 +1,21 @@
+#' Determines If Number Is Even Or Odd
+#' @param numb a numeric
+#' @return boolean
+#' @export
+#'
+isodd <- function(numb)
+{
+  if(class(numb) != "numeric")
+  {
+    stop("~~~input must benumeric, please try again~~~")
+  }else{
+    if((i %% 2) != 0)
+    {
+      judgement <- T
+    }else{
+      judgement <- F
+    }
+  }
+  return(judgement)
+}
+#(c) John Michael Adrian Wojahn
